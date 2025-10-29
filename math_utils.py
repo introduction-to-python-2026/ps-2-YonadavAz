4046c757cb72c6077b6c9452e4f955e18c13da71
YonadavAz <104059408+YonadavAz@users.noreply.github.com>
2025-10-29 14:53:13 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,17 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 >= num2 and num1 >= num3:
+         my_max_number = num1  # Replace 'pass' with code
+    if num2 >= num1 and num2 >= num3:
+        my_max_number = num2
+    if num3 >= num1 and num3 >= num2:
+        my_max_number = num3
+    return my_max_number  # Replace 'pass' with code
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    return (num1 + num2 + num3)/3  # Replace 'pass' with code
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
+    
     pass  # Replace 'pass' with code
 
